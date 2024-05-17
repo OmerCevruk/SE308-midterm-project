e0b03bbe4e56d06c44027459690b82b603670018
OmerCevruk <omer.cevrukk@gmail.com>
2024-05-17 20:58:30 +0300

    ready for remote

--- src/main/java/com/dbcourse/dbproject/MainView.java
+++ src/main/java/com/dbcourse/dbproject/MainView.java
@@ -11,31 +11,33 @@ public class MainView {
 
     @FXML
     private void initialize() {
+        // Create and configure the table columns
         TableColumn<ResultData, Integer> typeAUsersCol = new TableColumn<>("Number of Type A Users");
         typeAUsersCol.setCellValueFactory(new PropertyValueFactory<>("typeAUsers"));
 
         TableColumn<ResultData, Integer> typeBUsersCol = new TableColumn<>("Number of Type B Users");
         typeBUsersCol.setCellValueFactory(new PropertyValueFactory<>("typeBUsers"));
 
-        TableColumn<ResultData, Double> typeADeadlocksCol = new TableColumn<>("Duration of Deadlocks (Type A)");
-        typeADeadlocksCol.setCellValueFactory(new PropertyValueFactory<>("typeADeadlocks"));
-
-        TableColumn<ResultData, Double> typeBDeadlocksCol = new TableColumn<>("Duration of Deadlocks (Type B)");
-        typeBDeadlocksCol.setCellValueFactory(new PropertyValueFactory<>("typeBDeadlocks"));
-
-        TableColumn<ResultData, Integer> typeAThreadsCol = new TableColumn<>("Type A Threads Encountered by Type B Users");
-        typeAThreadsCol.setCellValueFactory(new PropertyValueFactory<>("typeAThreads"));
-
-        TableColumn<ResultData, Integer> typeBThreadsCol = new TableColumn<>("Type B Threads Encountered by Type A Users");
-        typeBThreadsCol.setCellValueFactory(new PropertyValueFactory<>("typeBThreads"));
-
         TableColumn<ResultData, Double> avgDurationTypeACol = new TableColumn<>("Average Duration of Type A Users");
         avgDurationTypeACol.setCellValueFactory(new PropertyValueFactory<>("avgDurationTypeA"));
+
+        TableColumn<ResultData, Integer> typeADeadlocksCol = new TableColumn<>("Number of Deadlocks (Type A)");
+        typeADeadlocksCol.setCellValueFactory(new PropertyValueFactory<>("typeADeadlocks"));
 
         TableColumn<ResultData, Double> avgDurationTypeBCol = new TableColumn<>("Average Duration of Type B Users");
         avgDurationTypeBCol.setCellValueFactory(new PropertyValueFactory<>("avgDurationTypeB"));
 
-        resultTable.getColumns().addAll(typeAUsersCol, typeBUsersCol, typeADeadlocksCol, typeBDeadlocksCol,
-                typeAThreadsCol, typeBThreadsCol, avgDurationTypeACol, avgDurationTypeBCol);
+        TableColumn<ResultData, Integer> typeBDeadlocksCol = new TableColumn<>("Number of Deadlocks (Type B)");
+        typeBDeadlocksCol.setCellValueFactory(new PropertyValueFactory<>("typeBDeadlocks"));
+
+        // Add the columns to the table
+        resultTable.getColumns().addAll(
+                typeAUsersCol,
+                typeBUsersCol,
+                avgDurationTypeACol,
+                typeADeadlocksCol,
+                avgDurationTypeBCol,
+                typeBDeadlocksCol
+        );
     }
 }
